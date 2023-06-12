5a8335c542b761407d2d2f1a431ef3e683e07d93
wendy <mizukilee@gmail.com>
2023-06-11 20:23:19 -0400

    Render instructions

--- config/puma.rb
+++ config/puma.rb
@@ -44,8 +44,6 @@
 
 
 
-# deploy start 
-
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -62,7 +60,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -74,13 +72,11 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 workers ENV.fetch("WEB_CONCURRENCY") { 4 }
-
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
 preload_app!
-
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
