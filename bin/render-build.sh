e4a9fa7b4965499414e8a8ec7554324729b0885d
kirby_son <someuser@duck.com>
2023-06-20 09:59:04 -0700

    added per docs

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,11 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# clean
+rm -rf public
+# build
+npm install --prefix client && npm run build --prefix client
+# migrate
 bundle exec rake db:migrate
+# postbuild
+cp -a client/build/. public/
